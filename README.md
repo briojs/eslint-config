712f39d93f7b124024fbd867cf1c5fef19284c0f
malezjaa <poreba.kris@gmail.com>
2024-06-04 22:51:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+## Ndst
+
+[![npm version](https://img.shields.io/npm/v/eslint-config-briojs?color=yellow)](https://npmjs.com/package/eslint-config-briojs)
+[![npm downloads](https://img.shields.io/npm/dm/eslint-config-briojs?color=yellow)](https://npmjs.com/package/eslint-config-briojs)
+[![bundle size](https://img.shields.io/bundlephobia/minzip/eslint-config-briojs?color=yellow)](https://bundlephobia.com/package/eslint-config-briojs)
+[![license](https://img.shields.io/github/license/briojs/eslint-config?color=yellow)](https://github.com/briojs/eslint-config/blob/main/LICENSE)
+
+### Install
+
+```sh
+# npm
+npm install -D eslint-config-briojs
+
+# yarn
+yarn add -D eslint-config-briojs
+
+# pnpm
+pnpm install -D eslint-config-briojs
+
+# bun
+bun install -D eslint-config-briojs
+```
+
+Published under the [MIT](https://github.com/briojs/eslint-config/blob/main/LICENSE) license.
+Made by [@malezjaa](https://github.com/briojs)
+and [community](https://github.com/briojs/eslint-config/graphs/contributors) 💛
+<br><br>
+<a href="https://github.com/briojs/eslint-config/graphs/contributors">
+<img src="https://contrib.rocks/image?repo=briojs/eslint-config" />
+</a>
+
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { config } from "typescript-eslint";
+import stylistic, { Rules } from "@stylistic/eslint-plugin";
+import tseslint from "typescript-eslint";
+// @ts-ignore
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+
+export interface Options {
+  rules?: Rules;
+}
+
+export default function eslintConfig(opts: Options) {
+  return config(
+    {
+      files: ["**/*.ts"],
+      ignores: ["dist/", "node_modules/**/*", "coverage/**/*"],
+      plugins: {
+        "@stylistic": stylistic,
+        unicorn: eslintPluginUnicorn,
+      },
+      // @ts-ignore
+      rules: {
+        "@stylistic/no-multiple-empty-lines": "error",
+        "@stylistic/no-floating-decimal": "error",
+        "@stylistic/no-extra-semi": "error",
+        "@stylistic/spaced-comment": "error",
+        "@stylistic/quotes": [
+          "error",
+          "single",
+          {
+            avoidEscape: true,
+          },
+        ],
+        "unicorn/no-null": "off",
+        "unicorn/no-process-exit": "off",
+        "unicorn/consistent-destructuring": "off",
+        "unicorn/no-array-reduce": "off",
+        "require-await": "off",
+        "multiline-ternary": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        ...opts.rules,
+      } as Rules,
+    },
+    ...tseslint.configs.recommended,
+    eslintPluginUnicorn.configs["flat/recommended"],
+  );
+}
